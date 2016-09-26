5e174604625c487d97ce5728da3a233966195ecd
Marissa Staller <marissastaller@Marissas-MacBook-Pro.local>
2016-09-26 09:51:53 -0400

    Most recent version of Lego storefront

--- JS Basics/1.0_theDOM.js
+++ JS Basics/1.0_theDOM.js
@@ -1,15 +1,18 @@
 //alert("HEY HEY HEY")
 
-var hobbiesList = document.getElementById("hobby-list");
+var hobbiesList = document.getElementsByClassName("hobby-list");
 console.log(hobbiesList);
 
-var hobbies = document.getElementsByClassName("hobby");
+var hobbies = document.getElementsByClassName("It-music");
 console.log(hobbies);//an array
 console.log(hobbies[0].innerHTML);
 console.log(hobbies[1].innerHTML);
 
 
 /* 
+
+var hobbiesList = document.getElementById
+var hobbies = document.getElementsByClassName
 var musicList = document.getElementById("It-music");
 console.log(musicList);
 
--- jQuery/0.00_jquery.js
+++ jQuery/0.00_jquery.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 		$("#image").hide("explode", { pieces:1000}, 5000);
 	});
 
-//
+//tried to do toggle effect from https://api.jqueryui.com/bounce-effect/
 	//$("#toggle").click(function() {
 	//	$("#image").toggle("bounce", { times:3}, "slow");//
 
